fc97bf27b2a172ad5c137cdb8fc70db3ad514a2d
Marko Mijatović <mijat5555@gmail.com>
2023-02-21 14:27:30 +0100

    EdunovaApp

---
+++ EdunovaAPP/src/main/java/edunova/Start01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+import edunova.util.PocetniInsert;
+
+/**
+ *
+ * @author Marko
+ */
+public class Start01 {
+
+    public static void main(String[] args) {
+        PocetniInsert.izvedi();
+    }
+    }
+
